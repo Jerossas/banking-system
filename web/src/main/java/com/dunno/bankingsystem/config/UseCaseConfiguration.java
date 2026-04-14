1e6de08059de67fcf5bf050d22636e290b369f7b
Joan Ferley Mosquera Lozano <mosqueralozanojhoanferley@gmail.com>
2026-04-13 19:21:56 -0500

    fix(web): add use case beans configuration class
    - Added RegisterUserUseCase use case bean

---
+++ web/src/main/java/com/dunno/bankingsystem/config/UseCaseConfiguration.java
@@ -0,0 +1,23 @@
+package com.dunno.bankingsystem.config;
+
+import com.dunno.bankingsystem.repositories.AccountRepository;
+import com.dunno.bankingsystem.repositories.UserRepository;
+import com.dunno.bankingsystem.services.PasswordEncoder;
+import com.dunno.bankingsystem.usecases.user.RegisterUserUseCase;
+import com.dunno.bankingsystem.usecases.user.RegisterUserUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UseCaseConfiguration {
+
+    @Bean
+    public RegisterUserUseCase registerUserUseCase(
+            UserRepository userRepository,
+            AccountRepository accountRepository,
+            PasswordEncoder passwordEncoder
+    ){
+        return new RegisterUserUseCaseImpl(userRepository, accountRepository, passwordEncoder);
+    }
+
+}
